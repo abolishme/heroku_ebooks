d7fe0ac56164c40ec22e4ceadbc0993abd93f6fd
abolishme <ahwoooga@gmail.com>
2015-04-09 11:38:14 -0400

    decreased odds

--- local_settings.py
+++ local_settings.py
@@ -9,7 +9,7 @@ MY_ACCESS_TOKEN_KEY = '3102187408-VfmE7a4Q70bodBwiZ3Y1B56c1V2DB14kOE44gAV'
 MY_ACCESS_TOKEN_SECRET = '9KK9pw4d08dyaZv7QHs39I46hq0tlGJ4qOsGKZUTwNws8'
 
 SOURCE_ACCOUNTS = ['mask_mag', 'abolishme', 'izzynastasia', 'hannahurr'] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
-ODDS = 2 #How often do you want this to run? 1/8 times?
+ODDS = 5 #How often do you want this to run? 1/8 times?
 ORDER = 3 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
 DEBUG = False #Set this to False to start Tweeting live
 STATIC_TEST = False #Set this to True if you want to test Markov generation from a static file instead of the API.
