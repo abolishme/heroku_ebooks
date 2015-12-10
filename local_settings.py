5e351d5d808d0c63d576c3695b1a8d46900087d8
abolishme <ahwoooga@gmail.com>
2015-12-09 21:05:24 -0500

    adjusted odds

--- local_settings.py
+++ local_settings.py
@@ -8,10 +8,10 @@ MY_CONSUMER_SECRET = 'Ie0dYNOABbFeT18xm44PdPAXLuCGwHkM1aj6Go4DEAuYgpmegY'
 MY_ACCESS_TOKEN_KEY = '3102187408-VfmE7a4Q70bodBwiZ3Y1B56c1V2DB14kOE44gAV'
 MY_ACCESS_TOKEN_SECRET = '9KK9pw4d08dyaZv7QHs39I46hq0tlGJ4qOsGKZUTwNws8'
 
-SOURCE_ACCOUNTS = ['mask_mag', 'abolishme', 'izzynastasia', 'hannahurr', 'yzplz'] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
-ODDS = 4 #How often do you want this to run? 1/8 times?
-ORDER = 3 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
-DEBUG = False #Set this to False to start Tweeting live
-STATIC_TEST = False #Set this to True if you want to test Markov generation from a static file instead of the API.
-TEST_SOURCE = ".txt" #The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API.
-TWEET_ACCOUNT = "maskintern" #The name of the account you're tweeting to.
+SOURCE_ACCOUNTS = ['mask_mag', 'abolishme', 'izzynastasia', 'hannahurr', 'yzplz']  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+ODDS = 6  # How often do you want this to run? 1/8 times?
+ORDER = 3  # how closely do you want this to hew to sensical? 1 is low and 3 is high.
+DEBUG = False  # Set this to False to start Tweeting live
+STATIC_TEST = False  # Set this to True if you want to test Markov generation from a static file instead of the API.
+TEST_SOURCE = ".txt"  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API.
+TWEET_ACCOUNT = "maskintern"  # The name of the account you're tweeting to.
